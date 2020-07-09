48a05e0e6c38339bf6f05f992f3e8fbc3f13547b
Einhander <49888382+HyperionTR@users.noreply.github.com>
2020-07-08 22:07:28 -0500

    Add files via upload

--- src/Modelo/Cita.java
+++ src/Modelo/Cita.java
@@ -8,47 +8,19 @@ public class Cita {
 	private int hora;
 	private Paciente paciente;
 	private Medico medico;
+	private ArrayList<Receta> recetas;
+	private Diagnostico diagnostico
 
 	public Cita(Calendar fecha,
 				int hora,
 				Paciente paciente,
-				Medico medico){
-		this.setFecha(fecha);
-		this.setHora(hora);
-		this.setPaciente(paciente);
-		this.setMedico(medico);
-	}
-
-	public Calendar getFecha() {
-		return fecha;
-	}
-
-	public void setFecha(Calendar fecha) {
-		this.fecha = fecha;
-	}
-
-	public int getHora() {
-		return hora;
-	}
-
-	public void setHora(int hora) {
-		this.hora = hora;
-	}
-
-	public Paciente getPaciente() {
-		return paciente;
-	}
-
-	public void setPaciente(Paciente paciente) {
-		this.paciente = paciente;
-	}
-
-	public Medico getMedico() {
-		return medico;
-	}
-
-	public void setMedico(Medico medico) {
-		this.medico = medico;
+				Medico medico,
+				ArrayList<Receta> recetas){
+		this.fecha=fecha;
+		this.hora=hora;
+		this.paciente=paciente;
+		this.medico=medico;
+		this.recetas=recetas;
 	}
 	
 	
